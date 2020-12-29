34c2e806e39ebec0b3baa7fde123ffd60bb3c396
karlalan <hkiomdeagmia@hotmail.com>
2020-12-29 11:15:26 +0900

    issue #17

--- src/app/syslog/syslog.component.spec.ts
+++ src/app/syslog/syslog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SyslogComponent } from './syslog.component';
 
@@ -6,7 +6,7 @@ describe('SyslogComponent', () => {
   let component: SyslogComponent;
   let fixture: ComponentFixture<SyslogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SyslogComponent ]
     })
